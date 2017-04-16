9cdc73728ebb5d998dcfad26bfc4ea515da49bab
Z. T. Zhang <web@cmhoc.ca>
2017-04-16 17:05:18 -0400

    new file:   .babelrc 	modified:   .gitignore 	deleted:    config.js 	deleted:    main.js

--- config.js
+++
@@ -1,9 +0,0 @@
-export {
-  reddit: {
-    userAgent: 'USER_AGENT',
-    clientId: 'CLIENT ID',
-    clientSecret: 'CLIENT SECRET',
-    username: 'BOT USERNAME',
-    password: 'BOT PASSWORD'
-  }
-}
